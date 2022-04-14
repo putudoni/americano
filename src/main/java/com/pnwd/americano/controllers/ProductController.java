b20183470fc999e9d0b4b800916c3accd7cbad26
putudoni <putuwigadoni@yahoo.com>
2022-04-14 11:05:44 +0700

    Create Product Rest Controller

---
+++ src/main/java/com/pnwd/americano/controllers/ProductController.java
@@ -0,0 +1,22 @@
+package com.pnwd.americano.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pnwd.americano.repositories.ProductRepository;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@GetMapping
+	public ResponseEntity<?> list(){
+		return ResponseEntity.ok(productRepository.findAll());
+	}
+}
