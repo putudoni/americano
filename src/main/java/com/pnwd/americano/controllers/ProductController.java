db4383e713cb01455cbd50630bafeb7b4a89ea36
putudoni <putuwigadoni@yahoo.com>
2022-04-16 14:43:36 +0700

    Call Method findByName ProductRepository in ProductController

--- src/main/java/com/pnwd/americano/controllers/ProductController.java
+++ src/main/java/com/pnwd/americano/controllers/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pnwd.americano.repositories.ProductRepository;
@@ -20,8 +21,8 @@ public class ProductController {
 	private ProductRepository productRepository;
 	
 	@GetMapping
-	public ResponseEntity<?> list(){
-		return ResponseEntity.ok(productRepository.findById(1L).get().getStocksLessThanFive());
+	public ResponseEntity<?> list(@RequestParam("name") String name){
+		return ResponseEntity.ok(productRepository.findByNameContaining(name));
 	}
 	
 }
