aaa77c837ac0ab3c7852f5902f53999d24d97161
putudoni <putuwigadoni@yahoo.com>
2022-04-19 14:00:55 +0700

    Add Price in Product Entity

--- src/main/java/com/pnwd/americano/models/Product.java
+++ src/main/java/com/pnwd/americano/models/Product.java
@@ -1,10 +1,12 @@
 package com.pnwd.americano.models;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -50,6 +52,9 @@ public class Product extends AbstractAggregateRoot<Product> implements Serializa
 	private String name;
 
 	private String brand;
+	
+	@Column(precision = 10, scale = 2)
+	private BigDecimal price;
 
 	@JsonManagedReference
 	@OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "product")
